1ed4baffb6653599c1c4c7e28ebfd7246e6b3235
DudiAM19 <dudiali@gmail.com>
2021-07-03 22:48:53 +0700

    update absentpage

--- src/scenes/Absent/index.js
+++ src/scenes/Absent/index.js
@@ -1,14 +1,65 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Container } from 'components';
+import { View, Text, ScrollView, TouchableOpacity, FlatList } from 'react-native';
+import { Container, Icon } from 'components';
 import Styles from './styles';
 
+const DATA = [
+    {
+        namaKelas: '1. 12D Morning Regular'
+    },
+    {
+        namaKelas: '2. 12D Morning Regular'
+    },
+    {
+        namaKelas: '3. 12D Morning Regular'
+    },
+    {
+        namaKelas: '4. 12D Morning Regular'
+    },
+    {
+        namaKelas: '5. 12D Morning Regular'
+    },
+    {
+        namaKelas: '5. 12D Morning Regular'
+    },
+    {
+        namaKelas: '5. 12D Morning Regular'
+    },
+]
+
+const Item = (props) => {
+    return (
+        <ScrollView>
+            <TouchableOpacity style={Styles.dataSection}>
+                <Text style={Styles.textData}>{props.namaKelas}</Text>
+                <Icon name="ios-chevron-forward-sharp" type="Ionicons" style={Styles.icon} />
+            </TouchableOpacity>
+        </ScrollView>
+    )
+}
+
 const Absent = () => {
-    return(
+    return (
         <Container backgroundColor="white">
             <View style={Styles.header}>
-                 <Text style={Styles.textHeader}>Choose class</Text>
+                <Text style={Styles.textHeader}>Choose class</Text>
             </View>
+            <View style={Styles.contentSection}>
+                <Text style={Styles.textContent}>Class list</Text>
+            </View>
+            <FlatList
+                data={DATA}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <Item
+                        namaKelas={item.namaKelas}
+                    />
+                )}
+            />
+
+            <TouchableOpacity style={Styles.addBtn}>
+                <Text style={Styles.add}>+</Text>
+            </TouchableOpacity>
         </Container>
     )
 }
--- src/scenes/Absent/styles.js
+++ src/scenes/Absent/styles.js
@@ -16,5 +16,55 @@ export default StyleSheet.create({
         color: Colors.WHITE,
         fontFamily: Fonts.PMedium,
         fontSize: RFValue(26)
+    },
+    contentSection: {
+        width: width,
+        height: width * .1,
+        // backgroundColor: 'red',
+        marginTop: width * .2,
+        marginBottom: width * .08,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: Colors.BLACK_TEXT
+    },
+    textContent: {
+        fontFamily: Fonts.PMedium,
+        fontSize: RFValue(20),
+        color: Colors.BLACK_TEXT
+    },
+    dataSection: {
+        // backgroundColor: 'red',
+        height: width * .1,
+        marginHorizontal: width * .09,
+        borderBottomWidth: 1,
+        borderBottomColor: Colors.GRAY,
+        marginTop: width * .05,
+        flexDirection: 'row',
+    },
+    textData: {
+        fontFamily: Fonts.PRegular,
+        fontSize: RFValue(18),
+        color: Colors.BLACK_TEXT,
+        marginHorizontal: width * .05,
+        flex: 1
+    },
+    icon: {
+        fontSize: RFValue(25),
+        color: Colors.BLACK_TEXT
+    },
+    addBtn: {
+        width: width * .2,
+        height: width * .2,
+        backgroundColor: Colors.PRIMARY,
+        borderRadius: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: width * .7,
+        marginVertical: width * .1
+    },
+    add: {
+        fontSize: RFValue(50),
+        color: Colors.WHITE,
     }
 })
